90596bbaab9d872bbe80e5f5ac36590e4825a310
elaina231 <1067626592@qq.com>
2024-12-08 20:39:09 +0800

    try cath

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -11,7 +11,7 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // TODO: what if args is empty?
         if (args.length == 0) {
             System.out.println("Please enter a command.");
@@ -24,7 +24,11 @@ public class Main {
                     System.out.println("Incorrect operands.");
                     System.exit(0);
                 }
-                Repository.init();
+                try {
+                    Repository.init();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
                 break;
             case "add":
                 if (!Tree.GITLET_DIR.exists()) {
